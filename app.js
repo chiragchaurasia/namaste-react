6d541b81f48965f5ddc16eb87b7a035ebc0704cc
chiragkumar04 <thechiragchaurasia@gmail.com>
2023-11-25 19:37:50 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title", //style | null | id:"title", className:"class"
+  },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2] // children (react elements - object)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // override root(html)
+//passing a react element inside the root
+
+//async defer
+root.render(container);
